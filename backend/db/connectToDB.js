58c7b824e37d53a31d67097508d0dfc3e3fae267
the-ayushm <ayushm1247@gmail.com>
2025-04-09 00:01:02 +0530

    Removed hardcoded MongoDB URI and used environment variables

--- backend/db/connectToDB.js
+++ backend/db/connectToDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectToMongoDb = async () => {
     try {
-        await mongoose.connect("mongodb+srv://ayushml1247:Ayush123@cluster0.ad6uo.mongodb.net/chat-app-db?retryWrites=true&w=majority")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDB")
     }catch(err){
         console.log("Error connecting to MongoDB", err)
